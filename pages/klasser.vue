0159e317434e4632f3b09e8f8ab69ab2754698ee
ARPCharlie <charlie@arp-lan.com>
2025-01-13 22:03:25 +0100

    Feat: Home page + pages

---
+++ pages/klasser.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-full min-h-screen absolute top-0 -z-10 bg-slate-800"></div>
+    <div class="w-full min-h-screen text-white flex flex-row justify-center items-center space-x-20">
+        <div class="w-96 h-96 bg-gradient-to-br from-lime-400 to-green-500 p-10 rounded-full">
+            <div class="h-full w-full bg-white p-10"></div>
+        </div>
+        <div class="w-96 h-96 bg-gradient-to-br from-blue-700 to-purple-600 p-10 rounded-full">
+            <div class="h-full w-full bg-white p-10"></div>
+        </div>
+        <div class="w-96 h-96 bg-gradient-to-br from-red-700 to-orange-500 p-10 rounded-full">
+            <div class="h-full w-full bg-white p-10"></div>
+        </div>
+    </div>
+</template>

0b7ec0b4675f169b8b97af4003966dcfe346cd6f
ARPCharlie <charlie@arp-lan.com>
2025-01-15 13:07:05 +0100

    Feat: admin dir

---
+++ pages/admin/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <form @submit.prevent="handleLogin">
+            <div>
+                <label for="username">Username:</label>
+                <input type="text" id="username" v-model="username" required />
+            </div>
+            <div>
+                <label for="password">Password:</label>
+                <input type="password" id="password" v-model="password" required />
+            </div>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                username: '',
+                password: '',
+            };
+        },
+        methods: {
+            handleLogin() {
+
+            },
+        },
+    };
+</script>

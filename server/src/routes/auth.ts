76eedc9269a41d7ff0bf2257a61befc807680aa0
blinkelys <martinustomrenslemmen@gmail.com>
2025-01-26 00:06:15 +0100

    Feat: JWT siging

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,10 +1,9 @@
 import express from 'express'
 import user from '../models/user';
 import bcrypt from 'bcrypt';
-import { Op } from 'sequelize';
+import jwt from 'jsonwebtoken';
 const router = express.Router()
 import { checkAdmin } from '../middleware/auth';
-
 
 router.post('/login', async (req: express.Request, res: express.Response) => {
     console.log('Login attempt initiated');
@@ -15,15 +14,25 @@ router.post('/login', async (req: express.Request, res: express.Response) => {
         console.log('Searching for user with email:', data.email);
         const foundUser = await user.findOne({ email: data.email.toLowerCase() });
 
-
         if (foundUser) {
             console.log('User found:', foundUser.email);
             console.log('Validating password...');
             const isPasswordValid = await bcrypt.compare(data.password, foundUser.password);
+            // Successfully found the user, now validating the password
             if (isPasswordValid) {
                 console.log('Password validation successful');
                 console.log('Login successful for user:', foundUser.email);
-                res.status(200).json(foundUser);
+
+                // Generate a JWT token with the user's role and ID
+                const token = jwt.sign(
+                    { id: foundUser._id, role: foundUser.role },
+                    'your-secret-key',
+                    { expiresIn: '1h' }
+                );
+
+                // Respond with token and role
+                res.status(200).json({ token, role: foundUser.role });
+            // Error handling
             } else {
                 console.log('Password validation failed for user:', foundUser.email);
                 res.status(401).json({ message: 'Unauthorized' });
@@ -37,6 +46,7 @@ router.post('/login', async (req: express.Request, res: express.Response) => {
         res.status(500).json({ message: 'Internal Server Error' });
     }
 
+    // Default response
     console.log('Login attempt completed');
 });
 

75924bca24b231b9d37920c96e0277de634125eb
blinkelys <martinustomrenslemmen@gmail.com>
2025-03-10 14:01:10 +0100

    Feat: company sect component ref

--- client/pages/admin/dashboard.vue
+++ client/pages/admin/dashboard.vue
@@ -9,6 +9,9 @@
   <div class="w-full min-h-screen bg-gray-900 text-white flex flex-col space-y-6 items center justify-center p-4">
     <AdminNewsSection />
   </div>
+  <div class="w-full min-h-screen bg-gray-900 text-white flex flex-col space-y-6 items center justify-center p-4">
+    <AdminCompanySection />
+  </div>
 </template>
 
 <script>

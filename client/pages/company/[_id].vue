bd3e4e46270d089b9af395a0685a8585474a4ed6
blinkelys <martinustomrenslemmen@gmail.com>
2025-04-24 17:54:32 +0200

    Fix: company news cards not loading

--- client/pages/company/[_id].vue
+++ client/pages/company/[_id].vue
@@ -26,7 +26,7 @@
             </svg>
           </div>
         </div>
-        <CompanyNewsSection companyId="company.id" v-if="company" />
+          <CompanyNewsSection :companyId="company.id" v-if="company" />
         <div class="prose max-w-none" v-html="marked(company.companyContent || '')"></div>
     </div>
   </div>

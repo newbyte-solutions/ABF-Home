48f4ecba2e4e715cedbd68f3df5d7ff34d42f038
blinkelys <martinustomrenslemmen@gmail.com>
2025-05-04 22:30:08 +0200

    Fix: logout BTN styling

--- client/pages/admin/dashboard.vue
+++ client/pages/admin/dashboard.vue
@@ -7,16 +7,16 @@
     <RegisterCompany />
     <CreateArticleAdmin />
   </div>
-  <div class="w-full min-h-screen bg-gray-900 text-white flex flex-col space-y-6 items center justify-center p-4">
+  <div class="w-full min-h-screen bg-gray-900 text-white flex flex-col space-y-6 items-center justify-center p-4">
     <AdminNewsSection />
   </div>
-  <div class="w-full min-h-screen bg-gray-900 text-white flex flex-col space-y-6 items center justify-center p-4">
+  <div class="w-full min-h-screen bg-gray-900 text-white flex flex-col space-y-6 items-center justify-center p-4">
     <AdminCompanySection />
   </div>
-  <div class="w-full min-h-screen bg-gray-900 text-white flex flex-col space-y-6 items center justify-center p-4">
+  <div class="w-full min-h-screen bg-gray-900 text-white flex flex-col space-y-6 items-center justify-center p-4">
     <AdminUserManager />
   </div>
-  <div class="w-full min-h-screen bg-gray-900 text-white flex flex-col space-y-6 items center justify-center p-4">
-    <LogoutButton />
+  <div class="w-full h-fit bg-gray-900 text-white flex flex-col space-y-6 items-center justify-center p-4 pb-14">
+    <LogoutButton class="py-3 md:py-4 w-64 md:w-96"/>
   </div>
 </template>
